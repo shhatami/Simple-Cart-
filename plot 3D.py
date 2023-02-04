4ecd92f7adbfae3fcdb2aacaf93007ed7db26a84
Shani.ha <53007257+shhatami@users.noreply.github.com>
2023-02-04 15:15:58 +0330

    Create plot 3D.py

---
+++ plot 3D.py
@@ -0,0 +1,50 @@
+def Plot_3D(X, X_test, y_test, clf, x1, x2, mesh_size, margin):
+            
+    # Specify a size of the mesh to be used
+    mesh_size=mesh_size
+    margin=margin
+
+    # Create a mesh grid on which we will run our model
+    x_min, x_max = X.iloc[:, 0].fillna(X.mean()).min() - margin, X.iloc[:, 0].fillna(X.mean()).max() + margin
+    y_min, y_max = X.iloc[:, 1].fillna(X.mean()).min() - margin, X.iloc[:, 1].fillna(X.mean()).max() + margin
+    xrange = np.arange(x_min, x_max, mesh_size)
+    yrange = np.arange(y_min, y_max, mesh_size)
+    xx, yy = np.meshgrid(xrange, yrange)
+            
+    # Calculate predictions on grid
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    Z = Z.reshape(xx.shape)
+
+    # Create a 3D scatter plot with predictions
+    fig = px.scatter_3d(x=X_test[x1], y=X_test[x2], z=y_test,
+                     opacity=0.8, color_discrete_sequence=['black'])
+
+    # Set figure title and colors
+    fig.update_layout(#title_text="Scatter 3D Plot with CART Prediction Surface",
+                      paper_bgcolor = 'white',
+                      scene = dict(xaxis=dict(title=x1,
+                                              backgroundcolor='white',
+                                              color='black',
+                                              gridcolor='#f0f0f0'),
+                                   yaxis=dict(title=x2,
+                                              backgroundcolor='white',
+                                              color='black',
+                                              gridcolor='#f0f0f0'
+                                              ),
+                                   zaxis=dict(title='Probability of Rain Tomorrow',
+                                              backgroundcolor='lightgrey',
+                                              color='black', 
+                                              gridcolor='#f0f0f0', 
+                                              )))
+    
+    # Update marker size
+    fig.update_traces(marker=dict(size=1))
+
+    # Add prediction plane
+    fig.add_traces(go.Surface(x=xrange, y=yrange, z=Z, name='CART Prediction',
+                              colorscale='Jet',
+                              reversescale=True,
+                              showscale=False, 
+                              contours = {"z": {"show": True, "start": 0.5, "end": 0.9, "size": 0.5}}))
+    fig.show()
+    return fig
